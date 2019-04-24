f7a2b117563b68ad86b5a66633c86dd86b96d77a
Chris Pak <chris.pak125@gmail.com>
2019-04-24 14:58:43 -0400

    Created handleSubmit and handleChange functions within Sarch component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Results from './Results';
+
+const ACCESS_KEY = `${process.env.REACT_APP_IMAGE_ACCESS_KEY}`
 
 export default class Search extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    state = {
+        searchQuery: "",
+        searchResults: []
+    }
+
+    handleChange = (e) => {
+        this.setState({searchQuery: e.target.value})
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await axios.get(`https://api.unsplash.com/search/photos/?client_id=${ACCESS_KEY}&query=${this.state.searchQuery}}`)
+        // console.log(response)
+        this.setState({searchResults: response.data.results})
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.searchQuery}
+                        placeholder="Search for Images..." onChange={this.handleChange} >
+                    </input>
+                    <button type-="submit">Search</button>
+                </form>
+
+                <Results results={this.state.searchResults} />
+            </div>
+        )
+    }
 }
